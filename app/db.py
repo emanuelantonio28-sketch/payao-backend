8158c95c6b10e4c650a1962bdb2cd1331128394d
emanuelantonio28-sketch <emanuel.antonio28@gmail.com>
2025-11-03 17:07:45 +0100

    Update and rename db.py.txt to db.py

--- app/db.py
+++ app/db.py
@@ -12,3 +12,4 @@ DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
